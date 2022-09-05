821d9af5f47f6d5c7259b991d26356fd55c8b71e
ChaoRen <1259664861@qq.com>
2022-09-05 18:48:01 +0800

    hot-fix commit by idea

--- src/main/java/com/gitTest/testGit.java
+++ src/main/java/com/gitTest/testGit.java
@@ -4,5 +4,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("dawdwa");
         System.out.println("git2 git2");
+        System.out.println("hot-fix merge---");
     }
 }
